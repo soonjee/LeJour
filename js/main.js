acc8b0346395a92b70fe9ea40a5a1aca433d8e3d
soonji <youinxxs@naver.com>
2022-08-23 17:46:57 +0900

    0823

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const main_slider = new Swiper('.main_slider', {
         loop: true,
-        speed: 3500,
+        speed: 3500, //넘어가는 속도, autoplay에 넣었다가 안 먹혔었음.
         autoplay: {
             delay: 3000,
             disableOnInteraction: false,
